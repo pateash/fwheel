e3dba4231b4aae48827398d9ed272c033ba55cd6
Ashish Patel <ashishpatel0720@gmail.com>
2024-07-26 14:46:11 +0530

    files: working

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,12 +4,13 @@ from click.testing import CliRunner
 from fwheel.cli import build
 
 parent_directory = Path(__file__).parent
-TEST_PROJECT_PATH = (parent_directory / "resources" / "ashish-libs")
+TEST_PROJECT_PATH = parent_directory / "resources" / "ashish-libs"
 
 
 def test_fwheel():
     runner = CliRunner()
-    print("\n",TEST_PROJECT_PATH)
-    result = runner.invoke(build, ["-p", "ashish_libs", str(TEST_PROJECT_PATH)], catch_exceptions=False)
+    print("\n", TEST_PROJECT_PATH)
+    result = runner.invoke(
+        build, ["-p", "ashish_libs", str(TEST_PROJECT_PATH)], catch_exceptions=False
+    )
     print(result.output)
-
